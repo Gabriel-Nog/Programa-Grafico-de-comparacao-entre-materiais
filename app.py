0b0e7fc9d496f768b8e7bfb6259d157c81076391
Gabriel Nogueira <81097897+Gabriel-Nog@users.noreply.github.com>
2024-10-07 11:26:13 +0000

    Atualização de legendas
    Novas legendas para densidade em app.py

--- app.py
+++ app.py
@@ -4,29 +4,40 @@ import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
+
 # Csv file
 def read_and_process_csv(file_path):
     try:
         df = pd.read_csv(file_path, encoding='utf-8')
-        print("Colunas no DataFrame:", df.columns)  # Adicione esta linha para verificar os nomes das colunas
-        
-        required_columns = ['Massa Específica (g/cm³)', 'Módulo de elasticidade(Gpa)']
+        print("Colunas no DataFrame:", df.columns
+              )  # Adicione esta linha para verificar os nomes das colunas
+
+        required_columns = [
+            'Massa Específica (g/cm³)', 'Módulo de elasticidade(Gpa)'
+        ]
         for col in required_columns:
             if col not in df.columns:
                 raise ValueError(f"Coluna {col} não encontrada no arquivo CSV")
-        
+
         df = df.dropna(subset=required_columns)
-        df['Massa Específica (g/cm³)'] = df['Massa Específica (g/cm³)'].str.replace(',', '.').astype(float)
-        df['Módulo de elasticidade(Gpa)'] = df['Módulo de elasticidade(Gpa)'].astype(float)
-        df['Coeficiente de Poisson'] = df['Coeficiente de Poisson'].str.replace(',', '.').astype(float)
-        df['Coeficiente de expansão térmica(10^-6 °C^-1)'] = df['Coeficiente de expansão térmica(10^-6 °C^-1)'].str.replace(',', '.').astype(float)
-        df['Calor específico J/kg.K'] = df['Calor específico J/kg.K'].astype(float)
+        df['Massa Específica (g/cm³)'] = df[
+            'Massa Específica (g/cm³)'].str.replace(',', '.').astype(float)
+        df['Módulo de elasticidade(Gpa)'] = df[
+            'Módulo de elasticidade(Gpa)'].astype(float)
+        df['Coeficiente de Poisson'] = df[
+            'Coeficiente de Poisson'].str.replace(',', '.').astype(float)
+        df['Coeficiente de expansão térmica(10^-6 °C^-1)'] = df[
+            'Coeficiente de expansão térmica(10^-6 °C^-1)'].str.replace(
+                ',', '.').astype(float)
+        df['Calor específico J/kg.K'] = df['Calor específico J/kg.K'].astype(
+            float)
         df['Densidade'] = df['Densidade'].astype(float)
         return df
     except Exception as e:
         print(f"Erro ao ler e processar o arquivo CSV: {e}")
         return None
-      
+
+
         # //plot_coeficiente_poisson
 def plot_coeficiente_poisson(df, materials_list):
     if materials_list[0] == '' and materials_list[1] == '':
@@ -34,7 +45,7 @@ def plot_coeficiente_poisson(df, materials_list):
             top_8 = df.nlargest(8, 'Coeficiente de Poisson')
             materials = top_8['Material']
             coeficiente_poisson = top_8['Coeficiente de Poisson']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, coeficiente_poisson, color='salmon')
             plt.xlabel('Coeficiente de Poisson')
@@ -49,10 +60,11 @@ def plot_coeficiente_poisson(df, materials_list):
     else:
         try:
             df_filtered = df[df['Material'].isin(materials_list)]
-            df_filtered = df_filtered.sort_values(by='Coeficiente de Poisson', ascending=False)
+            df_filtered = df_filtered.sort_values(by='Coeficiente de Poisson',
+                                                  ascending=False)
             materials = df_filtered['Material']
             coeficiente_poisson = df_filtered['Coeficiente de Poisson']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, coeficiente_poisson, color='salmon')
             plt.xlabel('Coeficiente de Poisson')
@@ -65,19 +77,25 @@ def plot_coeficiente_poisson(df, materials_list):
         except Exception as e:
             print(f"Erro ao plotar Coeficiente de Poisson: {e}")
 
+
 # //plot_coeficiente_expansao_termica
 def plot_coeficiente_expansao_termica(df, materials_list):
     if materials_list[0] == '' and materials_list[1] == '':
         try:
-            top_8 = df.nlargest(8, 'Coeficiente de expansão térmica(10^-6 °C^-1)')
+            top_8 = df.nlargest(
+                8, 'Coeficiente de expansão térmica(10^-6 °C^-1)')
             materials = top_8['Material']
-            coeficiente_expansao_termica = top_8['Coeficiente de expansão térmica(10^-6 °C^-1)']
-            
+            coeficiente_expansao_termica = top_8[
+                'Coeficiente de expansão térmica(10^-6 °C^-1)']
+
             plt.figure(figsize=(10, 8))
-            plt.barh(materials, coeficiente_expansao_termica, color='lightcoral')
+            plt.barh(materials,
+                     coeficiente_expansao_termica,
+                     color='lightcoral')
             plt.xlabel('Coeficiente de expansão térmica(10^-6 °C^-1)')
             plt.ylabel('Material')
-            plt.title('Top 8 Maiores Coeficientes de Expansão Térmica dos Materiais')
+            plt.title(
+                'Top 8 Maiores Coeficientes de Expansão Térmica dos Materiais')
             plt.grid(axis='x', linestyle='--', alpha=0.7)
             plt.tight_layout()
             plt.savefig('static/images/graph_expansao.png')
@@ -87,21 +105,28 @@ def plot_coeficiente_expansao_termica(df, materials_list):
     else:
         try:
             df_filtered = df[df['Material'].isin(materials_list)]
-            df_filtered = df_filtered.sort_values(by='Coeficiente de expansão térmica(10^-6 °C^-1)', ascending=False)
+            df_filtered = df_filtered.sort_values(
+                by='Coeficiente de expansão térmica(10^-6 °C^-1)',
+                ascending=False)
             materials = df_filtered['Material']
-            coeficiente_expansao_termica = df_filtered['Coeficiente de expansão térmica(10^-6 °C^-1)']
-            
+            coeficiente_expansao_termica = df_filtered[
+                'Coeficiente de expansão térmica(10^-6 °C^-1)']
+
             plt.figure(figsize=(10, 8))
-            plt.barh(materials, coeficiente_expansao_termica, color='lightcoral')
+            plt.barh(materials,
+                     coeficiente_expansao_termica,
+                     color='lightcoral')
             plt.xlabel('Coeficiente de expansão térmica(10^-6 °C^-1)')
             plt.ylabel('Material')
-            plt.title('Coeficientes de Expansão Térmica dos Materiais Selecionados')
+            plt.title(
+                'Coeficientes de Expansão Térmica dos Materiais Selecionados')
             plt.grid(axis='x', linestyle='--', alpha=0.7)
             plt.tight_layout()
             plt.savefig('static/images/graph_expansao.png')
             plt.close()
         except Exception as e:
             print(f"Erro ao plotar Coeficiente de Expansão Térmica: {e}")
+
 
 # //plot_calor_especifico
 def plot_calor_especifico(df, materials_list):
@@ -110,7 +135,7 @@ def plot_calor_especifico(df, materials_list):
             top_8 = df.nlargest(8, 'Calor específico J/kg.K')
             materials = top_8['Material']
             calor_especifico = top_8['Calor específico J/kg.K']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, calor_especifico, color='gold')
             plt.xlabel('Calor específico J/kg.K')
@@ -125,10 +150,11 @@ def plot_calor_especifico(df, materials_list):
     else:
         try:
             df_filtered = df[df['Material'].isin(materials_list)]
-            df_filtered = df_filtered.sort_values(by='Calor específico J/kg.K', ascending=False)
+            df_filtered = df_filtered.sort_values(by='Calor específico J/kg.K',
+                                                  ascending=False)
             materials = df_filtered['Material']
             calor_especifico = df_filtered['Calor específico J/kg.K']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, calor_especifico, color='gold')
             plt.xlabel('Calor específico J/kg.K')
@@ -140,6 +166,8 @@ def plot_calor_especifico(df, materials_list):
             plt.close()
         except Exception as e:
             print(f"Erro ao plotar Calor Específico: {e}")
+
+
 # //plot_densidade
 def plot_densidade(df, materials_list):
     if materials_list[0] == '' and materials_list[1] == '':
@@ -147,10 +175,10 @@ def plot_densidade(df, materials_list):
             top_8 = df.nlargest(8, 'Densidade')
             materials = top_8['Material']
             densidade = top_8['Densidade']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, densidade, color='plum')
-            plt.xlabel('Densidade')
+            plt.xlabel('Densidade g/cm³')
             plt.ylabel('Material')
             plt.title('Top 8 Maiores Densidades dos Materiais')
             plt.grid(axis='x', linestyle='--', alpha=0.7)
@@ -162,13 +190,14 @@ def plot_densidade(df, materials_list):
     else:
         try:
             df_filtered = df[df['Material'].isin(materials_list)]
-            df_filtered = df_filtered.sort_values(by='Densidade', ascending=False)
+            df_filtered = df_filtered.sort_values(by='Densidade',
+                                                  ascending=False)
             materials = df_filtered['Material']
             densidade = df_filtered['Densidade']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, densidade, color='plum')
-            plt.xlabel('Densidade')
+            plt.xlabel('Densidade g/cm³')
             plt.ylabel('Material')
             plt.title('Densidades dos Materiais Selecionados')
             plt.grid(axis='x', linestyle='--', alpha=0.7)
@@ -178,7 +207,6 @@ def plot_densidade(df, materials_list):
         except Exception as e:
             print(f"Erro ao plotar Densidade: {e}")
 
-        
 
 # //plot_massa_especifica
 def plot_massa_especifica(df, materials_list):
@@ -187,7 +215,7 @@ def plot_massa_especifica(df, materials_list):
             top_8 = df.nlargest(8, 'Massa Específica (g/cm³)')
             materials = top_8['Material']
             massa_especifica = top_8['Massa Específica (g/cm³)']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, massa_especifica, color='skyblue')
             plt.xlabel('Massa Específica (g/cm³)')
@@ -202,10 +230,11 @@ def plot_massa_especifica(df, materials_list):
     else:
         try:
             df_filtered = df[df['Material'].isin(materials_list)]
-            df_filtered = df_filtered.sort_values(by='Massa Específica (g/cm³)', ascending=False)
+            df_filtered = df_filtered.sort_values(
+                by='Massa Específica (g/cm³)', ascending=False)
             materials = df_filtered['Material']
             massa_especifica = df_filtered['Massa Específica (g/cm³)']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, massa_especifica, color='skyblue')
             plt.xlabel('Massa Específica (g/cm³)')
@@ -217,6 +246,8 @@ def plot_massa_especifica(df, materials_list):
             plt.close()
         except Exception as e:
             print(f"Erro ao plotar Massa Específica: {e}")
+
+
 # //plot_modulo_elasticidade
 def plot_modulo_elasticidade(df, materials_list):
     if materials_list[0] == '' and materials_list[1] == '':
@@ -224,7 +255,7 @@ def plot_modulo_elasticidade(df, materials_list):
             top_8 = df.nlargest(8, 'Módulo de elasticidade(Gpa)')
             materials = top_8['Material']
             modulo_elasticidade = top_8['Módulo de elasticidade(Gpa)']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, modulo_elasticidade, color='lightgreen')
             plt.xlabel('Módulo de elasticidade(Gpa)')
@@ -239,10 +270,11 @@ def plot_modulo_elasticidade(df, materials_list):
     else:
         try:
             df_filtered = df[df['Material'].isin(materials_list)]
-            df_filtered = df_filtered.sort_values(by='Módulo de elasticidade(Gpa)', ascending=False)
+            df_filtered = df_filtered.sort_values(
+                by='Módulo de elasticidade(Gpa)', ascending=False)
             materials = df_filtered['Material']
             modulo_elasticidade = df_filtered['Módulo de elasticidade(Gpa)']
-            
+
             plt.figure(figsize=(10, 8))
             plt.barh(materials, modulo_elasticidade, color='lightgreen')
             plt.xlabel('Módulo de elasticidade(Gpa)')
@@ -253,13 +285,13 @@ def plot_modulo_elasticidade(df, materials_list):
             plt.savefig('static/images/graph_modulo.png')
             plt.close()
         except Exception as e:
-            print(f"Erro ao plotar Módulo de Elasticidade: {e}")       
+            print(f"Erro ao plotar Módulo de Elasticidade: {e}")
 
-    
 
 @app.route('/')
 def form():
     return render_template('form.html')
+
 
 @app.route('/generate-graph', methods=['POST'])
 def generate_graph():
@@ -269,19 +301,23 @@ def generate_graph():
 
     # Loop para pegar todos os elementos até que não haja mais entradas
     while f'elemento{i}' in request.form:
-        materials_list.append(request.form[f'elemento{i}'])    
+        materials_list.append(request.form[f'elemento{i}'])
         i += 1
-    
+
     selected_options = request.form.getlist('options')
     df = read_and_process_csv('BaseDeDados.csv')
     options = []
     for option in selected_options:
         options.append(option)
-    
+
     if df is not None:
         if not selected_options:
-            return render_template('form.html', error="Nenhuma opção foi selecionada. Por favor, selecione pelo menos uma opção.")
-        
+            return render_template(
+                'form.html',
+                error=
+                "Nenhuma opção foi selecionada. Por favor, selecione pelo menos uma opção."
+            )
+
         if 'massa' in selected_options:
             plot_massa_especifica(df, materials_list)
         if 'modulo' in selected_options:
@@ -294,8 +330,11 @@ def generate_graph():
             plot_calor_especifico(df, materials_list)
         if 'densidade' in selected_options:
             plot_densidade(df, materials_list)
-    
-    return render_template('view.html', selected_options=options, materials_list=materials_list)
+
+    return render_template('view.html',
+                           selected_options=options,
+                           materials_list=materials_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
